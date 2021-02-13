f76c4c0cd2301010d7a851152a3f302764f827e9
Wesley <wspencer3295@yahoo.com>
2021-02-12 19:08:17 -0700

    commit tree 44b174087de5abd17598e854cc7cf46e68ba365e

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Wes is the champion";
+newHeader.class = "victory";
